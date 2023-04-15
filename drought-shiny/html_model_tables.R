89ef2d5fd878e395bdfd92891470583149f7c49e
MaxMLang <langmaxmelchior@gmail.com>
2023-04-16 00:10:50 +0200

    Added task, summaries and edf1 vars

--- drought-shiny/html_model_tables.R
+++ drought-shiny/html_model_tables.R
@@ -3,32 +3,62 @@ library(parameters)
 library(tidyverse)
 library(mgcv)
 library(performance)
-
+# Penalised Splines below (1.5 EDF will be modelled as linear)
 # Full Model ----
 # Winter ----
-# 20203 (Precip, infiltration, snowdrain) ----
+# 20203 (Precip, infiltration, snowdrain)
 
-# 11502 (precip, relhum, max_precip, glorad) ----
+# 11502 (precip, relhum, max_precip, glorad)
 
-# 10304 (airtmp, infiltration) ----
-
-
+# 10304 (airtmp, infiltration)
 
 # Selected 1 ----
+# 20203 (NONE)
+
+# 11502 (avg_glorad)
+
+# 10304 (NONE)
 
 # Selected 2 ----
+# 20203 (airtmp, snowstorage, infiltration) 
+
+# 11502 (relhum, precip)
+
+# 10304 (groundwaterdepth)
+
 
 # Interactions ----
+# 20203 (airtmp, snowstorage, infiltration)
+
+# 11502 (relhum, precip)
+
+# 10304 (groundwaterdepth)
 
 # Summer ----
-# 20203 (Precip, infiltration, snowdrain) ----
+# Full Model ----
+# 20203 (Precip, infiltration, snowdrain)
 
-# 11502 (precip, relhum, max_precip, glorad) ----
+# 11502 (precip, relhum, max_precip, glorad
 
-# 10304 (airtmp, infiltration) ----
+# 10304 (airtmp, infiltration)
 
-# Selected 1 ----
+# Selected 1 (Nota copy paste error snowstorage always linear) ---- 
+# 20203 (snowstorage) 
+
+# 11502 (snowstorage)
+
+# 10304 (snowstorage)
 
 # Selected 2 ----
+# 20203 (airtmp snowstorage)
+
+# 11502 (snowstorage)
+
+# 10304 (avg_relhum, snowstorage)
 
 # Interactions ----
+# 20203 (infiltration)
+
+# 11502 (relhum, snowstorage)
+
+# 10304 (groundwaterdepth, snowstorage, relhum)
