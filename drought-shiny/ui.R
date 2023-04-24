5100bc110ad6fa06246d868033dc5209b5760c18
MaxMLang <langmaxmelchior@gmail.com>
2023-04-24 15:09:20 +0200

    Adjusted More Tab with new exp.

--- drought-shiny/ui.R
+++ drought-shiny/ui.R
@@ -31,6 +31,7 @@ tags$style(HTML("
         padding: 10px;
       }"))
 
+
 shinyUI(fluidPage(
   navbarPage(id = "navbar", title = div(img(src='icon.png', style="background-color: transparent; margin-top: -10px;", height = 35), tags$a(href= "https://github.com/StatPrak-Droughts", "Niedrigwasser BY")),
                theme = shinytheme("lumen"),
@@ -156,6 +157,7 @@ Des Weiteren zeigt der Pegel eine Exposition von 275.7, was bedeutet, dass er ei
                                                        tabPanel("Summer",
                                                                 h1("Summer"),
                                                                 uiOutput("model_tab_summer"),
+                                                                p(strong("* p = 0.05 ** p = 0.01 *** p = 0.001 + p > 0.1 ")),
                                                                 p(strong("Hinweis:\n"), "Die Odds Ratios können bei den vergleichbaren Modellen (Full & Selected 1/2) bei unterschiedlichen Pegeln sehr extreme Werte annehmen. Eine Odds Ratio von 0 ist meistens durch Artefakte in den Daten zu erklären. Man sollte deshalb stets die Effekt und Rugplots betrachten. \n
                                              Wir haben uns dafür entschlossen die Variablen dennoch nicht zu skalieren, da bestimmte Treiber wie 'Snowstorage' im Norden auf der gegebenen Skala sehr kleine log. Odss annehmen, im Süden jedoch bei gleicher Skala eine sinnvolle Interpretation gewährleisten."),
                                                                 verbatimTextOutput("model_summary_summer"),
@@ -173,6 +175,9 @@ Des Weiteren zeigt der Pegel eine Exposition von 275.7, was bedeutet, dass er ei
                                                        tabPanel("Winter", 
                                                                 h1("Winter"),
                                                                 uiOutput("model_tab_winter"),
+                                                                p(strong("* p = 0.05 ** p = 0.01 *** p = 0.001 + p > 0.1 ")),
+                                                                p(strong("Hinweis:\n"), "Die Odds Ratios können bei den vergleichbaren Modellen (Full & Selected 1/2) bei unterschiedlichen Pegeln sehr extreme Werte annehmen. Eine Odds Ratio von 0 ist meistens durch Artefakte in den Daten zu erklären. Man sollte deshalb stets die Effekt und Rugplots betrachten. \n
+                                             Wir haben uns dafür entschlossen die Variablen dennoch nicht zu skalieren, da bestimmte Treiber wie 'Snowstorage' im Norden auf der gegebenen Skala sehr kleine log. Odss annehmen, im Süden jedoch bei gleicher Skala eine sinnvolle Interpretation gewährleisten."),
                                                                 verbatimTextOutput("model_summary_winter"),
                                                                 plotOutput("model_effect_winter_1"),
                                                                 plotOutput("model_effect_winter_2"),
@@ -217,32 +222,25 @@ Des Weiteren zeigt der Pegel eine Exposition von 275.7, was bedeutet, dass er ei
                           tabPanel("Über das Projekt", icon = icon("circle-question"),
                                    fluidRow(
                                        h1("Über das Projekt"),
-                                       p("Dieses Datenanalyse-Projekt beschäftigt sich mit der Vorhersage von Niedrigwasser für drei Pegel (Isar, Iller und Fränkische Saale) in Bayern. Das Projekt wurde als Weiterführung eines statistischen Consulting-Projekts von Theresa Meier und Nikita Paschan im Rahmen des statistischen Praktikums bearbeitet. Das Ziel des Projekts ist es, die Treiber von Niedrigwasser zu verstehen und Vorhersagen mit bestimmten Treibervariablen zu treffen. Im Consulting-Projekt wurde ganz Bayern mit deutlich mehreren Pegeln modelliert und vorgestellt. Die Daten stammen aus dem ClimEx-Projekt, einer Zusammenarbeit zwischen Bayern und Québec, das ein hydrologisches Simulationsmodell verwendet, um den Einfluss des Klimawandels auf meteorologische und hydrologische Extremereignisse in Bayern zu untersuchen. Es wurden 50 verschiedene Sätze von simulierten Daten (Mitglieder) mit unterschiedlichen Startbedingungen verwendet, um die natürliche Variabilität zu berücksichtigen. Das Projekt hat somit wichtige Implikationen für das Wasserressourcenmanagement in Bayern."),
+                                       p("Dieses Datenanalyse-Projekt beschäftigt sich mit der Vorhersage von Niedrigwasser für drei Pegel (Isar, Iller und Fränkische Saale) in Bayern. Das Projekt wurde als Weiterführung eines statistischen Consulting-Projekts von Theresa Meier und Nikita Paschan im Rahmen des statistischen Praktikums bearbeitet. Das Ziel des Projekts ist es, die Treiber von Niedrigwasser zu verstehen und Vorhersagen mit bestimmten Treibervariablen zu treffen. Im Consulting-Projekt wurde ganz Bayern mit deutlich mehreren Pegeln modelliert und vorgestellt. Die Daten stammen aus dem ClimEx-Projekt, einer Zusammenarbeit zwischen Bayern und Québec, das ein hydrologisches Simulationsmodell verwendet, um den Einfluss des Klimawandels auf meteorologische und hydrologische Extremereignisse in Bayern zu untersuchen. Es wurden 50 verschiedene Sätze von simulierten Daten (Member) mit unterschiedlichen Startbedingungen verwendet, um die natürliche Variabilität zu berücksichtigen. Das Projekt hat somit wichtige Implikationen für das Wasserressourcenmanagement in Bayern."),
                                        fluidRow(img(src = "Climex.png", height = 100, width = 250), img(src = "stablab.jpg", height = 150, width = 150))
                                        )       
                           ),
                           tabPanel("Modelle Leitfaden", icon = icon("lightbulb"),
                                    fluidRow(
                                      h1("Modelle Leitfaden"),
-                                     p("Generalisierte additive Modelle (GAMs) sind eine Art von Regressionsanalyse, die verwendet wird, um Beziehungen zwischen einer Antwortvariable und einer oder mehreren Prädiktorvariablen zu modellieren. In der binomialen Regression ist die Antwortvariable binär (z.B. 0 oder 1), und die Prädiktorvariablen können entweder kontinuierlich oder kategorial sein.
-Die Interpretation von GAM-Koeffizienten in der binomialen Regression erfolgt in folgenden Schritten:"),
-                                     h2("Lineare GAM-Koeffizienten:"),
-                                     p("1. Zunächst ist es wichtig zu verstehen, dass in einer binomialen Regression mit einem linearen GAM-Term der Koeffizient die Log-Odds der Antwortvariable für eine Einheitserhöhung der Prädiktorvariable darstellt, während alle anderen Prädiktorvariablen konstant gehalten werden."), br(),
-                                     p("2. Das Vorzeichen des Koeffizienten gibt die Richtung der Beziehung zwischen der Prädiktorvariable und der Antwortvariable an. Ein positiver Koeffizient zeigt an, dass eine Erhöhung der Prädiktorvariable mit einer Erhöhung der Log-Odds der Antwortvariable verbunden ist, während ein negativer Koeffizient das Gegenteil anzeigt."), br(),
-                                     p("3. Die Größe des Koeffizienten spiegelt die Stärke der Beziehung zwischen der Prädiktorvariable und der Antwortvariable wider. Je größer der Koeffizient, desto stärker ist der Effekt der Prädiktorvariable auf die Antwortvariable."),
-                                     h2("Spline GAM-Koeffizienten:"),
-                                     p("1. In einer binomialen Regression mit einem Spline GAM-Term stellt der Koeffizient die Log-Odds der Antwortvariable für eine Einheitserhöhung der Prädiktorvariable dar, während alle anderen Prädiktorvariablen konstant gehalten werden, aber der Effekt der Prädiktorvariable auf die Antwortvariable mit einer Spline-Funktion modelliert wird."), br(),
-                                     p("2. Die Interpretation des Koeffizienten in einem Spline GAM-Term ist etwas komplexer als in einem linearen Term. Der Koeffizient stellt den durchschnittlichen Effekt der Prädiktorvariable auf die Log-Odds der Antwortvariable über den Bereich der Prädiktorvariable dar."), br(),
-                                     p("3. Das Vorzeichen und die Größe des Koeffizienten geben nach wie vor die Richtung und Stärke der Beziehung zwischen der Prädiktorvariable und der Antwortvariable an."), br(),
-                                     h2("Dos & dont's"),
-                                     h3("Dos"),
-                                     p("1. Man sollte fachspezifisches Wissen verwenden, um die Koeffizienten zu interpretieren. Das Verständnis des Kontexts der Daten und der analysierten Variablen ist entscheidend für eine genaue Interpretation."), br(),
-                                     p("2. Koeffizienten derselben Skala bei exakt gleichem Modell. (Einflussvariablen) dürfen miteinander verlgichen werden. Es darf aber beispielweise nicht der Koeffizient des Trimmed Modells mit dem Koeffizienten des Full Modells verglichen werden, auch wenn bei die gleiche Skala besitzen."),
-                                     h3("Dont's"),
-                                     p("1. Es dürfen keine Effekte unterschiedlicher Einflussvariablen miteinander verglichen werden. Die Koeffizienten Beta hänger von der Skala der Einflussvariable ab."), br(),
-                                     p("2. Es dürfen keine Effekte der gleichen Einflussvariablen zwischen Modellen verglichen werden, wenn die Modelle mit unterschiedlichen Einflussvariablen gefittet worden sind."), br(),
-                                     p("3. Der p-Wert ist NICHT geeignet eine Aussage über die Stärke des Effekts zu treffen. Dazu sollte Fachwissen verwendet werden, um die Relevanz der Koeffizienten einzuordnen.")
-                                   )))
+                                     h2("Einführung in Generalisierte Lineare Modelle (GAMs)"),
+                                     img(src='GAM_Intro.png', align = "center", height = 450),
+                                     br(),
+                                     h2("Interpretation der Koeffizienten"),
+                                     img(src='interpretation.png', align = "center", height = 450),
+                                     br(),
+                                     h2("ROC Kurven und Prädiktion"),
+                                     img(src='ROC.png', align = "center", height = 430),
+                                     br(),
+                                     h2("AIC - Akaikes Informationskriterium"),
+                                     img(src='AIC.png', align = "center", height = 220),
+                                     br())))
     )
 )
 )
